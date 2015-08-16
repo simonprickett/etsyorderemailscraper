68e1ac63f1b987e1a30811c41ad28b2f552c766f
Simon Prickett <simon@crudworks.org>
2015-08-15 22:30:10 -0700

    Does the 2008-2011 data now.

--- parser.py
+++ parser.py
@@ -1,14 +1,48 @@
+import sys
+
 processingShippingAddress = False
+requestedAddress = 0
 
 with open('mbox') as fp:
 	for line in fp:
-		# TODO Date:
+		if (requestedAddress == 2):
+			sys.stdout.write(line.strip())
+			sys.stdout.write(",")
+			requestedAddress = 0
+		
+		if (requestedAddress == 1):
+			requestedAddress = 2
+		
+		if (line.startswith("The buyer has requested that the item be shipped to the following address.")):
+			requestedAddress = 1
+		
 		if (line.startswith("Order Total:") or line.startswith("Total:") or line.startswith("Total Sale:") or line.startswith("TOTAL SALE:")):
-			print line.replace("Order Total:", "").replace("Total:", "").replace("Total Sale:", "").replace("TOTAL SALE:", "").replace("(USD)", "").replace("(US)", "").strip()
+			dollarAmount = (line.replace("Order Total:", "").replace("Total:", "").replace("Total Sale:", "").replace("TOTAL SALE:", "").replace("(USD)", "").replace("(US)", "").replace("USD", "").strip())
+			dollarAmount = dollarAmount[dollarAmount.find("$"):]
+			dollarAmount = dollarAmount.replace("<br>", "").strip()
+			sys.stdout.write(dollarAmount)
+			sys.stdout.write(',')
+		
 		if (processingShippingAddress):
-			print line.strip()
+			buyerName = line.replace("<br>", "").strip()
+			
+			if (len(buyerName) > 0):
+				sys.stdout.write(buyerName)
+				sys.stdout.write(',')
+			else:
+				requestedAddress = 2
+
 			processingShippingAddress = False
+		
 		if (line.startswith("Shipping Address:") or line.startswith("The buyer has requested that the item be shipped to:")):
 			processingShippingAddress = True
-		if (line.startswith("* Email") or line.startswith(" * Email") or line.startswith("If you need to contact the buyer, you may do so by sending an email to:")):
-			print ((line.replace("* Email", "")).replace("If you need to contact the buyer, you may do so by sending an email to:", "")).strip()
+		
+		if (line.startswith(" * Send an email to ") or line.startswith("* Email") or line.startswith(" * Email") or line.startswith("If you need to contact the buyer, you may do so by sending an email to:") or line.startswith("If you would like to contact the buyer, send an email to")):
+			emailData = line.replace(" * Send an email to ", "").replace(" * Email:", "").replace("* Email", "").replace("If you need to contact the buyer, you may do so by sending an email to:", "").replace("If you would like to contact the buyer, send an email to", "").strip()
+			orStartPos = emailData.find("or start")
+			if (orStartPos > -1):
+				emailData = emailData[:orStartPos]
+
+			emailData = emailData.replace("<br>", "").replace("</a>", "").strip()
+			sys.stdout.write(emailData)
+			sys.stdout.write("\n")
